252a99fc4ab542b74510e86198b07ace87673e8a
hiiamalieny <wasabifriday@gmail.com>
2025-11-23 18:00:21 +0900

    Delete types.ts

--- types.ts
+++
@@ -1,8 +0,0 @@
-// Defines the structure of a single YUKIO item
-export interface YukioItem {
-  id: string;
-  name: string;
-  // Optional description or category can be added later
-  category?: string; 
-  imageUrl?: string;
-}
